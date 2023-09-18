3840bcfcb61744678df500b1d88d3690d254de24
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-18 11:41:10 +0300

    :lipstick:added home files and styled the home-text div

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.scss'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='home-text'>
+            <h2>Stays in Finland</h2>
+            <span>12+ stays</span>
+        </div>
+    </div>
+  )
+}
+
+export default Home
