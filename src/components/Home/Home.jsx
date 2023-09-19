8d7f103f646a93d87ec2b1eeaca9fe152cd5b591
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-19 12:59:01 +0300

    :lipstick: fetched data from the ,json file and styled the cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,46 @@
-import './Home.scss'
-import image1 from '../assets/img.jpeg'
-import { AiFillStar } from'react-icons/ai'
+import './Home.scss';
+import { AiFillStar } from 'react-icons/ai';
+import { useEffect, useState } from 'react';
 
 function Home() {
-    return (
+  const [stays, setStays] = useState([]);
+
+  useEffect(() => {
+    fetch('/stays.json') /** stays.json is in the public directory */
+      .then((response) => response.json())
+      .then((data) => setStays(data))
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); /** empty dependency array means this effect runs once when the component mounts*/ 
+
+  return (
     <div className='home'>
-        <div className='home-text'>
-            <h2>Stays in Finland</h2>
-            <span>12+ stays</span>
-        </div>
-        <div className="img-container">
-            <img src={image1} alt="" />
+      <div className='home-text'>
+        <h2>Stays in Finland</h2>
+        <span>12+ stays</span>
+      </div>
+      <div className="cards-container">
+        {stays.map((stay, index) => (
+          <div key={index} className="img-container">
+            <img src={stay.photo} alt="" />
             <div className="image-text">
-                <h4>Super host</h4>
-                <p>Entire apartment . 2 beds</p>
-                <AiFillStar className='rating'/>
-                <span>4.40</span>
+              {stay.superHost && <h6>Super host</h6>}
+              <p>
+                {stay.type === "Entire apartment" ? `${stay.type} . ${stay.beds} beds`: stay.type}
+              </p>
+              <div className="rating-container">
+                <AiFillStar className='rating' />
+                <span>{stay.rating}</span>
+              </div>
             </div>
-            <p>Stylish apartment in center of the city</p>
-        </div>
+            <p>{stay.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    )
+  );
 }
 
-export default Home
+export default Home;
+
