3067fd052e30d872afef55483d5e28923fea11e9
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-21 10:26:13 +0300

    :lipstick: styled the placeholder container

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import './Navbar.scss'
 import logo from '../assets/logo.png'
 import { BiSearchAlt2 } from'react-icons/bi'
+import { useState } from'react'
+import Placeholder from '../Placeholder/Placeholder'
 
 function Navbar() {
-  function onSearch() {
-    console.log('search')
-  }
+  const [showPlaceholder, setShowPlaceholder] = useState(false);
+
+  const handleSearchClick = () => {
+    setShowPlaceholder(!showPlaceholder);
+  };
 
   return (
     <div className='navbar'>
       <div className='logo'>
         <img src={logo} alt='' />
       </div>
-      <div onClick={onSearch} className="search-bar">
-        <span className="location">
-          Helsinki, Finland
-        </span>
-        <span className="guests">
-          Add guests
-        </span>
-          <BiSearchAlt2 className="search-icon" />
+      <div onClick={handleSearchClick} className="search-bar">
+        <span className="location">Helsinki, Finland</span>
+        <span className="guests">Add guests</span>
+        <BiSearchAlt2 className="search-icon" />
       </div>
+      {showPlaceholder && <div className="blurred-background"><Placeholder /></div>}
     </div>
   )
 }
